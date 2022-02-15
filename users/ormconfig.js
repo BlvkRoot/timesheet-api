f985094e0cd805a15805cdde49fabf297bd35406
blvkroot <hsalukamba17@gmail.com>
2022-02-15 16:22:00 +0100

    ormconfig file paths corrected.

--- users/ormconfig.js
+++ users/ormconfig.js
@@ -2,25 +2,25 @@
 
 module.exports = {
     "type": "postgres",
-    "url": "postgres://postgres:123456@localhost:5432/testdb",
+    "url": "postgres://postgres:123456@localhost:5432/timesheet-db",
     // "ssl": { rejectUnauthorized: false},
-    "synchronize": false,
+    "synchronize": true,
     "logging": false,
     "entities": [
-       "src/entities/**/{*.ts,*.js}"
+       "./entities/**/{*.ts,*.js}"
       //  "dist/entities/**/{*.ts,*.js}"
     ],
     "migrations": [
-       "src/database/migration/**/{*.ts,*.js}"
+       "./database/migration/**/{*.ts,*.js}"
       //  "dist/database/migration/**/{*.ts,*.js}"
     ],
     "subscribers": [
-       "src/subscriber/**/{*.ts,*.js}"
+       "./subscriber/**/{*.ts,*.js}"
       //  "dist/subscriber/**/{*.ts,*.js}"
     ],
     "cli": {
-       "entitiesDir": "src/entities",
-       "migrationsDir": "src/database/migration",
-       "subscribersDir": "src/subscriber"
+       "entitiesDir": "./entities",
+       "migrationsDir": "./database/migration",
+       "subscribersDir": "./subscriber"
     }
  } 
