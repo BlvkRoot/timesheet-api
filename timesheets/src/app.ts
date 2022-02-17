dd9b53fdfe2d5b2058b41bdf822c8cce164701db
blvkroot <hsalukamba17@gmail.com>
2022-02-17 22:29:38 +0100

    Cors added.

--- timesheets/src/app.ts
+++ timesheets/src/app.ts
@@ -1,11 +1,18 @@
 import express, { Request, Response } from 'express';
 import timesheetRoutes from './routes/register/timesheet.routes';
+import cors from 'cors';
 import './database';
 
 const app = express();
+const corsOptions = {
+    origin: ["http://localhost:3000"],
+    credentials: true
+}
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
+
+app.use(cors(corsOptions));
 
 app.get('/', async (request: Request, response: Response) => {
     return response.json({
