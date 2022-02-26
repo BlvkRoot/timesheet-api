bd91f9600bcae82c9cce48a834263e771ceaf48a
blvkroot <hsalukamba17@gmail.com>
2022-02-26 22:34:33 +0100

    express custom types created

--- users/src/middleware/AuthMiddleware.ts
+++ users/src/middleware/AuthMiddleware.ts
@@ -1,14 +1,22 @@
 import { NextFunction, Request, Response } from 'express';
+import authJson from '../config/auth.json';
+import { JwtPayload, verify } from 'jsonwebtoken';
 
 export const isAuthenticated = async (
   request: Request,
   response: Response,
   next: NextFunction
 ) => {
-  if (!request.cookies.timesheets_session)
+  const token = request.cookies.timesheets_session;
+  if (!token) {
     return response.status(401).json({
       error: 'Unauthenticated user',
       success: false,
     });
-  next();
+  }
+
+  verify(token, authJson.secret, (decoded: JwtPayload) => {
+    request.userId = decoded.userId; 
+    next();
+  });
 };
